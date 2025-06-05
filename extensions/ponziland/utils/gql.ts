c4587d39c826aeb33304f07d9fb0b7eee2125447
ZackAmes <zames6584@gmail.com>
2025-06-05 16:12:46 -0400

    try fix

--- extensions/ponziland/utils/gql.ts
+++ extensions/ponziland/utils/gql.ts
@@ -56,7 +56,7 @@ export const land_query = (address: string) => `query GetOwnedLands {
 export const query_lands_under_price = (price: number, token: string) => {
   let price_str = (BigInt(price) * BigInt(10 ** 18)).toString();
   return `query GetLandsUnderPrice {
-  ponziLandLandModels(where:{sell_priceLT:${price_str}, token_used:"${trimLeadingZeros(token)}"}){
+  ponziLandLandModels(where:{sell_priceLT:"${price_str}", token_used:"${trimLeadingZeros(token)}"}){
     edges{
       node{
         owner
